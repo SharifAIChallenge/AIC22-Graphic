e77f9f9b64e7cf17764ad20e490a6323a187d359
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2022-06-03 17:01:49 +0430

    - fix dependencies

--- Assets/LogHandler.cs
+++ Assets/LogHandler.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class LogHandler : MonoBehaviour
