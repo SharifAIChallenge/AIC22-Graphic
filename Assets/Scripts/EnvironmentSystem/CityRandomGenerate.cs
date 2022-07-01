7e1d9fe1c79b4ca01af1f7f92e951634006a1cdd
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2022-07-01 19:57:29 +0430

    city generator

--- Assets/Scripts/EnvironmentSystem/CityRandomGenerate.cs
+++ Assets/Scripts/EnvironmentSystem/CityRandomGenerate.cs
@@ -7,15 +7,21 @@ using Random = UnityEngine.Random;
 public class CityRandomGenerate : MonoBehaviour
 {
     [SerializeField] private List<GameObject> buildings = new();
+    [SerializeField] private GameObject groundChunk;
     [SerializeField] private Vector2 xBounds;
     [SerializeField] private Vector2 zBounds;
     [SerializeField] private float yOffset;
     [SerializeField] private float gridSize = 4;
     [SerializeField] private float overlapSize = 3;
 
+    [SerializeField] private Vector2 mapXBounds;
+    [SerializeField] private Vector2 mapZBounds;
+    
     private List<GameObject> instantiatedBuildings = new();
 
     public LayerMask layerMask;
+    public LayerMask islandAreaMask;
+    
     public int count;
     [SerializeField] private Transform buildingParent;
 
@@ -56,6 +62,8 @@ public class CityRandomGenerate : MonoBehaviour
 
     public void Generate()
     {
+        var blockSize = 7;
+        
         var xCount = (int) (xBounds.y - xBounds.x) / gridSize;
         var zCount = (int) (zBounds.y - zBounds.x) / gridSize;
 
@@ -66,6 +74,22 @@ public class CityRandomGenerate : MonoBehaviour
                 var x = xBounds.x + i * gridSize;
                 var z = zBounds.x + j * gridSize;
                 var pos = new Vector3(x, yOffset, z);
+                
+                if (Physics.OverlapBox(pos, Vector3.one * (overlapSize * 2), Quaternion.identity, islandAreaMask).Length == 0)
+                {
+                    continue;
+                }
+                
+                Instantiate(groundChunk, pos + Vector3.up * 0.3f, Quaternion.identity, buildingParent);
+
+                if(x < mapXBounds.x || x > mapXBounds.y || z < mapZBounds.x || z > mapZBounds.y)
+                {
+                    if(i % blockSize == 0 || j % blockSize == 0)
+                    {
+                        continue;
+                    }
+                }
+                
                 if (Physics.OverlapBox(pos, Vector3.one * overlapSize, Quaternion.identity, layerMask).Length > 0)
                 {
                     continue;
