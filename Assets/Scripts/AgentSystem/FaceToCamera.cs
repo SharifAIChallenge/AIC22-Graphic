c23133980f0c78c908905dfa08ed1c1422964d2d
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2022-07-17 00:41:50 +0430

    - agent scale on zoom

--- Assets/Scripts/AgentSystem/FaceToCamera.cs
+++ Assets/Scripts/AgentSystem/FaceToCamera.cs
@@ -6,6 +6,8 @@ public class FaceToCamera : MonoBehaviour
 {
     private Camera _cam;
 
+    private float defaultDistance = 150;
+    
     void Start()
     {
         _cam = Camera.main;
@@ -13,12 +15,17 @@ public class FaceToCamera : MonoBehaviour
 
     void Update()
     {
+        var p = Vector3.Project(_cam.transform.position - transform.position, transform.forward);
+        var a = p.magnitude / defaultDistance;
+        a = Mathf.Clamp(a, 0.8f, 10);
+        transform.localScale = Vector3.one * a;
+
         var temp = transform.eulerAngles;
         temp.y = _cam.transform.eulerAngles.y;
         transform.eulerAngles = temp;
 
         temp = transform.localEulerAngles;
-        temp.x = _cam.transform.localEulerAngles.x;
+        temp.x = _cam.transform.localEulerAngles.x / 1.5f;
         transform.localEulerAngles = temp;
     }
 }
